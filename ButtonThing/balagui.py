c07f4aef06d4344358da71a93e8327c6371a9be6
Suryan Isaac <suryanisaac@icloud.com>
2023-01-18 19:17:01 +0530

    Changed the thing to do a thing

--- ButtonThing/balagui.py
+++ ButtonThing/balagui.py
@@ -6,11 +6,12 @@ import os
 root=Tk()
 root.title("Timer thing")
 # root.configure(bg="#EBDBB2")
+root.attributes("-fullscreen", True)
 
 #Set up the main variable (counter) label
 var = StringVar()
 var.set("Hello World")
-a = Label(root, textvariable=var, font=("Arial",100))
+a = Label(root, textvariable=var, font=("Arial",300))
 a.place(relx=0.5, rely=0.5, anchor="center")
 
 # Handle the events of keypresses as Interrupts
@@ -27,7 +28,7 @@ def key_pressed(event):
     else:
         var.set("You Loose")
         b = Label(root, text="You stopped at " +f'{counter:.2f}' + " seconds", font=("Arial",60))
-        b.place(relx=0.5, rely=0.6, anchor="center")
+        b.place(relx=0.5, rely=0.7, anchor="center")
         c = Label(root, text="Press the button to try again", font=("Arial",50))
         c.place(relx=0.5, rely=0.8, anchor="center")
         # def continueKey(event):
@@ -36,6 +37,9 @@ def key_pressed(event):
         root.bind("<Key>",continueKey)
     root.mainloop()
 
+var.set("Ready?")
+root.update()
+time.sleep(2)
 #Main Counting Loop
 counter=0
 delayTime=0.01
